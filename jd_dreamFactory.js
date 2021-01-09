8d51c7495953383b674c4b6b8badc4a1b2cc5731
131421 <74951479+131421@users.noreply.github.com>
2021-01-10 00:45:39 +0800

    Update jd_dreamFactory.js

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -99,7 +99,7 @@ async function jdDreamFactory() {
   if (!$.unActive) return
   await getUserElectricity();
   await taskList();
-  await investElectric();
+//  await investElectric();
   await QueryHireReward();//收取招工电力
   await PickUp();//收取自家的地下零件
   await stealFriend();
